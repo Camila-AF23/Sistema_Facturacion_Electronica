369e5153a13a6b2487f00de47b9cc5797195028b
Camila-AF23 <camilaalarcon900@gmail.com>
2026-06-22 21:15:52 -0500

    .

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // La URL de tu NestJS en local
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
 
 export default api;
